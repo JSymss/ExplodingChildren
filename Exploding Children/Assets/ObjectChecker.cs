2efde00ff78be38b6c896a71bd7178b85dc6a163
Terrence Gregg <1013830@student.sae.edu.au>
2021-03-05 16:55:36 +1100

    item code
    Started making item check code. Needs much work

---
+++ Exploding Children/Assets/ObjectChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectChecker : MonoBehaviour
+{
+    public Type type;
+    public Colour colour;
+
+    public bool Check(Want want)
+    {
+        if(want.ref == colour || want.ref == type)
+        {
+            if (want.ref == colour && want.ref == type)
+            {
+                return true;
+            }
+                
+           if()
+            {
+                return true;
+            }
+
+        }
+        return false; 
+    }
+
+}
