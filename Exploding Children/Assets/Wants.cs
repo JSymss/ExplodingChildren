ace4e436259958db9004c297bc7c0a4057a3961c
Terrence Gregg <1013830@student.sae.edu.au>
2021-03-07 16:50:04 +1100

    child wants done and tied to speech bubble
    very rough at the moment

---
+++ Exploding Children/Assets/Wants.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Wants : MonoBehaviour
+{
+    public enum Type { Bear, Ball, Drink}
+    public enum Colour { Red, Orange, Blue}
+    public bool hasWant = false;
+
+    public Text itemText;
+
+
+    public Type wantType;
+    public Colour wantColour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(hasWant == false)
+        {
+            wantType = (Type)Random.Range(0, 3);
+            wantColour = (Colour)Random.Range(0, 3);
+            hasWant = true;
+
+            string t = wantType.ToString();
+            string c = wantColour.ToString();
+
+            itemText.text = c + " " + t;
+        }
+
+        
+    }
+}
