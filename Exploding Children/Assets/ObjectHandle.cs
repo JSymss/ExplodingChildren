22938272bb3f20e6e5534d5f70de8ba442675626
Terrence Gregg <1013830@student.sae.edu.au>
2021-03-07 14:39:25 +1100

    I hate gitfork at times

---
+++ Exploding Children/Assets/ObjectHandle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHandle : MonoBehaviour
+{
+    public Want want;
+    public ObjectList[] objects;
+
+    public void CheckObject()
+        foreach(ObjectList o in objects);
+        {
+        if(o.Check(want))
+        {
+public struct
+Type;
+    Colour;
+        }
+        
+     
+public enum Type (bear, ball, drink);
+public enum Colour (orange, red, blue);
+}
+
